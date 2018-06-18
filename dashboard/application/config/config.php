389bcf68a345c4e0fb2fe7de42d24dfa2ac5d984
Chrysant Meike Damiani <crs.chryst@gmail.com>
2018-06-18 23:26:12 +0700

    Change base URL

--- dashboard/application/config/config.php
+++ dashboard/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://devpanel.xyz/api/dashboard/';
+$config['base_url'] = 'http://devpanel.xyz/dashboard/';
 
 /*
 |--------------------------------------------------------------------------
