3cb009d89aa19eff33d0f9c165468d6e87f474f2
Chrysant Meike Damiani <crs.chryst@gmail.com>
2018-06-28 16:13:04 +0700

    updates

--- dashboard/application/models/Mymodel.php
+++ dashboard/application/models/Mymodel.php
@@ -223,7 +223,7 @@ class Mymodel extends CI_Model {
 	
 	public function selectfac2($table)
 	{
-	    $this->db->select('facility.ID_FACILITY_DB, category.NAME_FACILITY, category.CREATED, (select FULL_NAME from users where ID_USER = category.CREATED_BY) "NamaAC" , facility.UPDATED, (select FULL_NAME from users where ID_USER = category.UPDATED_BY) "NamaAU",  facility.PUBLISHED');
+	    $this->db->select('facility.ID_FACILITY_DB, facility.NAME_FACILITY, facility.CREATED, (select FULL_NAME from users where ID_USER = facility.CREATED_BY) "NamaAC" , facility.UPDATED, (select FULL_NAME from users where ID_USER = facility.UPDATED_BY) "NamaAU",  facility.PUBLISHED');
 	    $this->db->join('users','facility.CREATED_BY = users.ID_USER');
 		return $this->db->get($table);
 	}
